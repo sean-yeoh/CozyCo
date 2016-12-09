3a6cd7df1a04bf7eb259fbeece36fab05ec57fc1
nextacademy <sean.lee.sijian@gmail.com>
2016-12-09 15:58:27 +0800

    added basic models and controllers for all

---
+++ app/controllers/topic_controller.rb
@@ -0,0 +1,18 @@
+class TopicController < ApplicationController
+	def create
+  		@topic = Topic.new(params[:topic])
+ 		if @topic.save
+    		@topic = Topic.new(:name => params[:topic][:name], :last_poster_id => current_user.id, :last_post_at => Time.now, :forum_id => params[:topic][:forum_id])
+ 
+	    	if @post.save
+		      flash[:notice] = "Successfully created topic."
+		      redirect_to "/forums/#{@topic.forum_id}"
+		    else
+		      redirect :action => 'new'
+		    end
+		else
+		 render :action => 'new'
+		end
+	end
+end
+
