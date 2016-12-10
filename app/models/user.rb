ca1b7b76eebd52e26bf72eb43de2d93a37e32041
Sean Yeoh <sean.yeoh@hotmail.com>
2016-12-10 22:38:53 +0800

    Added mailboxer gem and font-awesome gem in gemfile. Added some methods in User.rb model. Added mailbox and conversation views. Added navbar in application.html.erb. Added mailbox, conversations, etc table in db from mailboxer. Added some helper methods in ApplicationHelper and MailboxHelper. Added some helper methods in ApplicationController.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
   include Clearance::User
+  # require 'mailboxer'
+  # include Mailboxer::Models::Messageable
+  extend Mailboxer::Models::Messageable::ActiveRecordExtension
+
+  acts_as_messageable
+
+  def mailboxer_name
+    self.email
+  end
+
+  def mailboxer_email(object)
+    self.email
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,13 +21,21 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
+  # mailbox folder routes
+  get "mailbox/inbox" => "mailbox#inbox", as: :mailbox_inbox
+  get "mailbox/sent" => "mailbox#sent", as: :mailbox_sent
+  get "mailbox/trash" => "mailbox#trash", as: :mailbox_trash
 
+  # conversations
+  resources :conversations do
+    member do
+      post :reply
+      post :trash
+      post :untrash
+    end
+  end
 
-
-
-    resources :topics do
-        resources :comments
-     end
-
-
+  resources :topics do
+    resources :comments
+  end
 end
