fbdd364b1c940a1ddf5883decdac68421150e81d
nextacademy <sean.lee.sijian@gmail.com>
2016-12-10 16:38:19 +0800

    IMPORTANT: deleted unessential migration files and modified topics and comments migration files

--- db/migrate/20161209072919_create_topics.rb
+++ db/migrate/20161209072919_create_topics.rb
@@ -2,9 +2,10 @@ class CreateTopics < ActiveRecord::Migration
   def change
     create_table :topics do |t|
       t.string :name
-      t.text :description
+      t.text :content
 
       t.timestamps null: false
     end
+    add_foreign_key :topics, :users
   end
 end
--- db/migrate/20161209080942_add_foreign_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignToPosts < ActiveRecord::Migration
-  def change
-    add_column :posts, :topic_id, :integer
-  end
-end
