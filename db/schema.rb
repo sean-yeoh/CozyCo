c5fd0333e4edd6498a267b9604f4b6d99b77d42f
Emma Archibald <archibald.ej@gmail.com>
2016-12-14 17:11:37 +0800

    Improved the events feature. Added new columns for location, POC and start time. Improved the show page to add the new columns. Fixed a problem with validations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214035618) do
+ActiveRecord::Schema.define(version: 20161214162630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 20161214035618) do
     t.datetime "updated_at", null: false
     t.date     "start_date"
     t.date     "end_date"
+    t.time     "start_time"
+    t.string   "location"
+    t.string   "POC"
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
